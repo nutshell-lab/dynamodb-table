d26bd243ebd4b4680bc8dd8da281af24eff6d199
Victor Rebiard--Crépin <victor.rebiardcrepin@gmail.com>
2019-08-23 12:05:17 +0200

    Updated types definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,39 +1,34 @@
-import { DocumentClient } from "aws-sdk/lib/dynamodb/document_client";
-
-export interface Item {
-  key: Object;
-  data: Object;
-}
+import { DocumentClient } from "aws-sdk/lib/dynamodb/document_client"
 
 declare function query(client: DocumentClient)
   : (table: String)
   => (options: DocumentClient.QueryInput)
-  => Promise<Item>;
+  => Promise<Object>;
 
   declare function get(client: DocumentClient)
   : (table: String)
   => (key: Object, options: DocumentClient.GetItemInput)
-  => Promise<Item>;
+  => Promise<Object>;
 
 declare function scan(client: DocumentClient)
   : (table: String)
   => (options: DocumentClient.ScanInput)
-  => Promise<Array<Item>>;
+  => Promise<Array<Object>>;
 
 declare function put(client: DocumentClient)
   : (table: String)
-  => (item: Item, options: DocumentClient.PutItemInput)
-  => Promise<Item>;
+  => (item: Object, options: DocumentClient.PutItemInput)
+  => Promise<Object>;
 
 declare function update(client: DocumentClient)
   : (table: String)
   => (key: Object, options: DocumentClient.UpdateItemInput)
-  => Promise<Item>;
+  => Promise<Object>;
 
 declare function remove(client: DocumentClient)
   : (table: String)
   => (key: Object, options: DocumentClient.DeleteItemInput)
-  => Promise<Item>;
+  => Promise<Object>;
 
 exports.query = query;
 exports.get = get;
