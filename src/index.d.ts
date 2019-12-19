c0ff34006787c6e6ac7ee74725895d465d67a119
cprice <cprice@tutanota.com>
2019-12-19 16:13:17 +0100

    fix typescript types

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,33 +2,33 @@ import { DocumentClient } from "aws-sdk/lib/dynamodb/document_client"
 
 declare function query(client: DocumentClient)
   : (table: String)
-  => (options: DocumentClient.QueryInput)
-  => Promise<Object>;
+  => (options?: DocumentClient.QueryInput)
+  => Promise<any>;
 
   declare function get(client: DocumentClient)
   : (table: String)
-  => (key: Object, options: DocumentClient.GetItemInput)
-  => Promise<Object>;
+  => (key: any, options?: DocumentClient.GetItemInput)
+  => Promise<any>;
 
 declare function scan(client: DocumentClient)
   : (table: String)
-  => (options: DocumentClient.ScanInput)
-  => Promise<Array<Object>>;
+  => (options?: DocumentClient.ScanInput)
+  => Promise<Array<any>>;
 
 declare function put(client: DocumentClient)
   : (table: String)
-  => (item: Object, options: DocumentClient.PutItemInput)
-  => Promise<Object>;
+  => (item: any, options?: DocumentClient.PutItemInput)
+  => Promise<any>;
 
 declare function update(client: DocumentClient)
   : (table: String)
-  => (key: Object, options: DocumentClient.UpdateItemInput)
-  => Promise<Object>;
+  => (key: any, options?: DocumentClient.UpdateItemInput)
+  => Promise<any>;
 
 declare function remove(client: DocumentClient)
   : (table: String)
-  => (key: Object, options: DocumentClient.DeleteItemInput)
-  => Promise<Object>;
+  => (key: any, options?: DocumentClient.DeleteItemInput)
+  => Promise<any>;
 
 exports.query = query;
 exports.get = get;
